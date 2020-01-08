5f5e8cd9997adcd6c46e2deb69ac096232d1d38e
Edwinluja <edwiinlujaa@gmail.com>
2020-01-08 00:42:03 -0600

    ciclos for 2 - c/problema

--- vid39.c
+++ vid39.c
@@ -55,13 +55,13 @@ void problema(){
 
 	for(i = 1; i<=n;i++)
 	{
-		for (k = i; k <= n; k++)		//
-		{								//-----*
-			printf(" ");				//----**
-		}								//---***
-		for (j = 1; j <=i; j++)			//--****
-		{								//-*****
-			printf("* ");				//
+		for (k = i; k <= n; k++)
+		{
+			printf(" ");
+		}
+		for (j = 1; j <=i; j++)
+		{
+			printf("* ");
 		}
 
 		printf(" \n");
